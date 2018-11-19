5c7548741d1fe34db40a1d94d87a96a14a92d733
Bruno Tissi <bruno.tissi@hotmail.com>
2018-11-18 22:58:00 -0200

    Commit inicial - criação do projeto, app, primeira view e mapeamento de urls

--- stocker_project/stocker/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-
-
-def index(request):
-    context_dict = {'boldmessage': "Crunchy, creamy, cookie, candy, cupcake!"}
-    return render(request, 'stocker/index.html', context=context_dict)
