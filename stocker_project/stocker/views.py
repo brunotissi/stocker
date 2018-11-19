a85c014ee26b865a3c5691707253c98b0dc5ec78
Bruno Tissi <bruno.tissi@hotmail.com>
2018-11-18 22:07:33 -0200

    Configuração de pasta de templates, static e media. Criação de template exemplo

--- stocker_project/stocker/views.py
+++ stocker_project/stocker/views.py
@@ -1,5 +1,6 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def index(request):
-    return HttpResponse("Rango says hey there partner!")
+    context_dict = {'boldmessage': "Crunchy, creamy, cookie, candy, cupcake!"}
+    return render(request, 'stocker/index.html', context=context_dict)
