4c8a82a47d5aca525f1d5c379cd4f4ee7f15812d
Ana <acarolinamat@gmail.com>
2018-11-20 11:21:52 -0200

    Alteração do template de cadastro de unidade e listagem de insumos

--- stocker_project/stocker/forms.py
+++ stocker_project/stocker/forms.py
@@ -28,6 +28,8 @@ class EditInsumoForm(forms.ModelForm):
 
 
 class UnidadeForm(forms.ModelForm):
+	nome = forms.CharField(label='Nome da Unidade de Medida', required=True, widget=forms.TextInput(attrs={'placeholder': 'Nome da Unidade de Medida'}))
+
 	class Meta:
 		model = Unidade
 		fields = ("nome",)
