d36125dfade59aebdd99aab73eca087bc090cb83
ruanpetschow <ruan.petschow@gmail.com>
2018-11-20 10:16:50 -0200

    Template formulário cad_insumo.html e form cad_insumo

--- stocker_project/stocker/forms.py
+++ stocker_project/stocker/forms.py
@@ -5,15 +5,20 @@ from .models import Insumo, Unidade
 
 class UserForm(forms.ModelForm):
 	password = forms.CharField(widget=forms.PasswordInput())
+
 	class Meta:
 		model = User
 		fields = ('username', 'email', 'password')
 
 
 class InsumoForm(forms.ModelForm):
+	nome = forms.CharField(label='Nome do Insumo', required=True, widget=forms.TextInput(attrs={'placeholder': 'Nome do Insumo'}))
+	quantidade = forms.IntegerField(label='Quantidade', required=True, widget=forms.TextInput(attrs={'placeholder': 'Quantidade'}))
+	unidade = forms.ModelChoiceField(label='Unidade de Medida', required=True, queryset=Unidade.objects.all())
+
 	class Meta:
 		model = Insumo
-		fields = ('nome', 'quantidade', 'unidade', )
+		fields = ('nome','quantidade', 'unidade', )
 
 
 class EditInsumoForm(forms.ModelForm):
