bc5734f4be8d57ddb01eb782e97c2d5f293ec1b0
Bruno Tissi <bruno.tissi@hotmail.com>
2018-11-24 00:22:30 -0200

    Testes unitários

--- stocker_project/stocker/tests/__init__.py
+++ stocker_project/stocker/tests/__init__.py
@@ -1,23 +1,2 @@
 from .test_models import ModelUnidadeTest, ModelInsumoTest
-#from .test_view import ViewsTest
 from .test_forms import UnidadeFormTest, InsumoFormTest
-# def add_cat(nome, quantidade):
-#     c = Insumo.objects.get_or_create(nome=nome)[0]
-#     c.quantidade = quantidade
-#     c.save()
-#     return c
-#
-#
-# class IndexViewTest(TestCase):
-#
-#     def test_index_view_sem_insumos(self):
-#         response = self.client.get(reverse('listar_insumos'))
-#         self.assertEqual(response.status_code, 200)
-#
-#     def test_index_view_with_categories(self):
-#         add_cat('test', 30)
-#         add_cat('temp', 1)
-#         add_cat('tmp', 4)
-#         add_cat('tmp test temp', 20)
-#         response = self.client.get(reverse('listar_insumos'))
-#         self.assertEqual(response.status_code, 200)
